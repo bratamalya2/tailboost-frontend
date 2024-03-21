a675aae5a737ea7d3a3b29e7e3046796ce8f1442
Bratamalya Das Gupta <bratamalya2@gmail.com>
2024-03-21 17:03:23 +0530

    fixed styling issues & added firebase credentials to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyAijvJMYAZ7FmddCkduU78QuQCjNjbL_2c',
-    authDomain: 'tailboost-frontend.firebaseapp.com',
-    projectId: 'tailboost-frontend',
-    storageBucket: 'tailboost-frontend.appspot.com',
-    messagingSenderId: '681338879807',
-    appId: '1:681338879807:web:c26af3195dc7d1ef021d8a'
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
